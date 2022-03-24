5c9e276ca3648932efa379dea77b49da8b2dffb7
miwe95 <miweiss95@gmail.com>
2022-03-24 20:53:45 +0100

    boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,26 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Graphics} from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: innerWidth,
+	height: innerHeight
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+const graphics = new Graphics();
+graphics.beginFill(0xffffff);
+graphics.drawRect(20, 50, 100, 80);
+graphics.endFill();
 
+app.stage.addChild(graphics);
+
+
+/*const clampy: Sprite = Sprite.from("clampy.png");
 clampy.anchor.set(0.5);
-
 clampy.x = app.screen.width / 2;
 clampy.y = app.screen.height / 2;
-
 app.stage.addChild(clampy);
+*/
+
