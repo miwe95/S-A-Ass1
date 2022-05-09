81e91a1e3c9e403f337cda9d7311420758b14a4a
miwe95 <miweiss95@gmail.com>
2022-05-09 13:14:48 +0200

    d

--- src/HierarchyEnemy.ts
+++ src/HierarchyEnemy.ts
@@ -32,21 +32,22 @@ export class HierarchyEnemy extends Container {
     //@ts-ignore
     updateMatrix(angle: number, position: vec2, parent_transformation_matrix: mat3 | null, dt) {
         mat3.fromRotation(this.rotation_matrix_, angle);
-        mat3.fromTranslation(this.translation_matrix_, vec2.fromValues(position[0], position[1]));
-       
+
+        if (parent_transformation_matrix != null)
+            mat3.fromTranslation(this.translation_matrix_, vec2.fromValues(position[0], position[1]));
+        else
+            mat3.fromTranslation(this.translation_matrix_, vec2.fromValues(0, 0));
+            
         //console.log(this.translation_matrix_);
-        mat3.multiply(this.transformation_matrix_, this.rotation_matrix_, this.translation_matrix_);
-        if(parent_transformation_matrix != null){
+        mat3.multiply(this.transformation_matrix_, this.translation_matrix_, this.rotation_matrix_);
+        if (parent_transformation_matrix != null) {
             mat3.multiply(this.transformation_matrix_, parent_transformation_matrix, this.transformation_matrix_);
-            let vec_ = vec3.fromValues(0,0,1);
+            let vec_ = vec3.fromValues(0, 0, 1);
             vec3.transformMat3(vec_, vec_, this.transformation_matrix_);
             this.sprite_.x += vec_[0] * 0.1 * dt / 1000;
-            this.sprite_.y += vec_[1] * 0.1 *  dt / 1000;
+            this.sprite_.y += vec_[1] * 0.1 * dt / 1000;
             this.sprite_.rotation = angle;
         }
-
-        
-
         //console.log("root rotation matrix: " + this.rotation_matrix_);
     }
 }
