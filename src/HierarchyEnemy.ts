db436f26a9ee2de1a4f4524c89e09e79783ea6f5
miwe95 <miweiss95@gmail.com>
2022-05-09 21:24:19 +0200

    d

--- src/HierarchyEnemy.ts
+++ src/HierarchyEnemy.ts
@@ -1,24 +1,34 @@
-import { Container, Sprite } from "pixi.js";
-import { mat3, vec2, vec3 } from "gl-matrix";
+import { Container, Graphics, Sprite } from "pixi.js";
+//import { mat3, vec2, vec3 } from "gl-matrix";
+import { Matrix } from 'ml-matrix';
 
 export class HierarchyEnemy extends Container {
 
     sprite_: Sprite;
-    rotation_matrix_: mat3;
-    translation_matrix_: mat3;
-    transformation_matrix_: mat3;
-    transformation_matrix_inverse_: mat3;
+    rotation_matrix_: Matrix;
+    translation_matrix_: Matrix;
+    transformation_matrix_: Matrix;
+    local_transformation_matrix: Matrix;
+    tmp_matrix: Matrix;
     child_: HierarchyEnemy | null;
+    point_: Graphics;
+    screen_width_: number;
+    screen_height_: number;
 
     constructor(screen_width: number, screen_heigth: number, scale: number, png: string, child: HierarchyEnemy | null) {
         super();
 
-        this.rotation_matrix_ = mat3.create();
-        this.translation_matrix_ = mat3.create();
-        this.transformation_matrix_ = mat3.create();
-        this.transformation_matrix_inverse_ = mat3.create();
+        this.rotation_matrix_ = Matrix.eye(3,3);
+        this.translation_matrix_ = Matrix.eye(3,3);
+        this.transformation_matrix_ = Matrix.eye(3,3);
+        this.local_transformation_matrix = Matrix.eye(3,3);
+        this.tmp_matrix = Matrix.eye(3,3);
+        this.tmp_matrix = Matrix.eye(3,3);
         this.child_ = child;
-
+        this.screen_width_ = screen_width;
+        this.screen_height_ = screen_heigth;
+        this.point_ = new Graphics();
+        this.point_.lineStyle(4, 0x5a5e5b, 1);
         this.sprite_ = Sprite.from(png);
         this.sprite_.x = screen_width / 2;
         this.sprite_.y = screen_heigth / 2;
@@ -27,27 +37,42 @@ export class HierarchyEnemy extends Container {
         this.sprite_.interactive = true;
         this.sprite_.scale.set(scale);
         this.addChild(this.sprite_);
+        this.addChild(this.point_);
     }
 
-    //@ts-ignore
-    updateMatrix(angle: number, position: vec2, parent_transformation_matrix: mat3 | null, dt) {
-        mat3.fromRotation(this.rotation_matrix_, angle);
-
-        if (parent_transformation_matrix != null)
-            mat3.fromTranslation(this.translation_matrix_, vec2.fromValues(position[0], position[1]));
-        else
-            mat3.fromTranslation(this.translation_matrix_, vec2.fromValues(0, 0));
-            
-        //console.log(this.translation_matrix_);
-        mat3.multiply(this.transformation_matrix_, this.translation_matrix_, this.rotation_matrix_);
+    updateMatrix(angle: number, position: Matrix | null, parent_transformation_matrix: Matrix | null) {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        this.rotation_matrix_ = new Matrix([[cos, sin, 0],[-sin, cos, 0],[0,0,1]]);
+        this.sprite_.rotation += angle;
         if (parent_transformation_matrix != null) {
-            mat3.multiply(this.transformation_matrix_, parent_transformation_matrix, this.transformation_matrix_);
-            let vec_ = vec3.fromValues(0, 0, 1);
-            vec3.transformMat3(vec_, vec_, this.transformation_matrix_);
-            this.sprite_.x += vec_[0] * 0.1 * dt / 1000;
-            this.sprite_.y += vec_[1] * 0.1 * dt / 1000;
-            this.sprite_.rotation = angle;
+            console.log("child1 parents trans mat" + parent_transformation_matrix);
+            this.local_transformation_matrix = this.translation_matrix_.mmul(this.rotation_matrix_);
+            this.transformation_matrix_ = parent_transformation_matrix.mmul(this.local_transformation_matrix);
+            //console.log(this.transformation_matrix_);
+            console.log("child1 transformation matrix " + this.transformation_matrix_);
+            let vec_ = new Matrix([[0,0,1]]);
+            vec_ = vec_.mmul(this.transformation_matrix_);
+            this.sprite_.x = vec_.get(0,0) + this.screen_height_ /2;
+            this.sprite_.y = vec_.get(0,1) + this.screen_width_ / 2;
+            //this.point_.drawCircle(this.sprite_.x, this.sprite_.y, 1);
+            console.log("child1 vec0 and vec1: " + vec_.get(0,0) + " " + vec_.get(0,1));
         }
+
+        else {
+            if (position != null) {
+                this.tmp_matrix = new Matrix([[1, 0, 0],[0, 1, 0],[100, 100, 1]]);
+                //console.log("root tmp " +    this.tmp_matrix);
+                //console.log("root rot" + this.rotation_matrix_);
+                this.transformation_matrix_ = this.tmp_matrix.mmul(this.rotation_matrix_);
+                //console.log("root transformation matrix" + this.transformation_matrix_);
+                //mat3.multiply(this.transformation_matrix_, this.translation_matrix_, this.rotation_matrix_ );
+            }
+        }
+
+
+
+
         //console.log("root rotation matrix: " + this.rotation_matrix_);
     }
 }
--- src/SceneHierarchy.ts
+++ src/SceneHierarchy.ts
@@ -1,4 +1,4 @@
-import { vec2 } from "gl-matrix";
+import Matrix from "ml-matrix";
 import { Container} from "pixi.js";
 import { HierarchyEnemy } from "./HierarchyEnemy";
 
@@ -10,6 +10,8 @@ export class SceneHierarchy extends Container{
     child2_: HierarchyEnemy;
     child3_: HierarchyEnemy;
     rotation_speed_: number;
+    rotation_angle_: number;
+    count_:number;
 
     constructor(screen_width: number, screen_height: number){
         super();
@@ -21,17 +23,24 @@ export class SceneHierarchy extends Container{
         this.addChild(this.child2_);
         this.addChild(this.child3_);
         this.addChild(this.root_);
-        this.rotation_speed_ = 1;
+        this.rotation_speed_ = 0.78539816339745;
+        this.rotation_angle_ = 0;
+        this.count_ = 0;
     }
 
     //@ts-ignore
     update(dt: number){
-        this.root_.sprite_.rotation += this.rotation_speed_ * dt / 1000; 
-        //this.root_.sprite_.x += 50 * dt / 1000;
-        this.root_.updateMatrix(this.root_.sprite_.rotation, vec2.fromValues(this.root_.sprite_.x, this.root_.sprite_.y), null, dt);
-
-        this.child1_.updateMatrix(this.root_.sprite_.rotation, vec2.fromValues(this.child1_.sprite_.x, this.child1_.sprite_.y), this.root_.transformation_matrix_, dt);
-        this.child2_.updateMatrix(this.child1_.sprite_.rotation, vec2.fromValues(this.child2_.sprite_.x, this.child2_.sprite_.y), this.child1_.transformation_matrix_, dt); 
-        this.child3_.updateMatrix(this.child2_.sprite_.rotation, vec2.fromValues(this.child3_.sprite_.x, this.child3_.sprite_.y), this.child2_.transformation_matrix_, dt); 
+        if(this.count_ !=2)
+        {
+            //this.root_.sprite_.rotation += this.rotation_speed_ * dt / 1000;
+            this.rotation_angle_ += this.rotation_speed_ * dt / 1000;
+            //console.log(this.rotation_speed_ * dt / 1000);
+            //this.root_.sprite_.x += 50 * dt / 1000;
+            this.root_.updateMatrix(this.rotation_angle_, new Matrix([]), null);
+            this.child1_.updateMatrix(this.rotation_angle_ , null, this.root_.transformation_matrix_);
+            this.child2_.updateMatrix(this.rotation_angle_, null, this.child1_.transformation_matrix_); 
+            this.child3_.updateMatrix(this.rotation_angle_ * 1.5, null, this.child2_.transformation_matrix_); 
+            this.count_ = 1;
+        }
     }
 }
