f9c39b38b6cdc3b323b2bd18783e9a715d77cfe4
miwe95 <miweiss95@gmail.com>
2022-05-10 20:37:14 +0200

    sd

--- src/SceneHierarchy.ts
+++ src/SceneHierarchy.ts
@@ -23,9 +23,9 @@ export class SceneHierarchy extends Container {
 
         this.screen_width = screen_width;
         this.screen_height = screen_height;
-        this.child3_ = new HierarchyEnemy(screen_width, screen_height, 0.02, "enemy.png", null);
-        this.child2_ = new HierarchyEnemy(screen_width, screen_height, 0.04, "enemy.png", this.child3_);
-        this.child1_ = new HierarchyEnemy(screen_width+500, screen_height, 0.09, "enemy.png", this.child2_);
+        this.child3_ = new HierarchyEnemy(screen_width, screen_height, 0.02, "enemy_green.png", null);
+        this.child2_ = new HierarchyEnemy(screen_width, screen_height, 0.04, "enemy_red.png", this.child3_);
+        this.child1_ = new HierarchyEnemy(screen_width+500, screen_height, 0.09, "enemy_black.png", this.child2_);
         this.root_ = new HierarchyEnemy(screen_width+500, screen_height, 0.1, "black_hole.png", this.child1_);
         this.addChild(this.child1_);
         this.addChild(this.child2_);
@@ -44,6 +44,7 @@ export class SceneHierarchy extends Container {
 
 
             this.rotation_angle_ = this.rotation_speed_ * dt / 1000;
+            this.root_.sprite_.rotation += this.rotation_angle_;
 
             let parent_transformation = mat3.fromValues(1, 0, 0, 0, 1, 0, this.root_.sprite_.x, this.root_.sprite_.y, 1);
 
@@ -52,14 +53,12 @@ export class SceneHierarchy extends Container {
             mat3.multiply(parent_transformation, parent_transformation, this.child1_.local_transformation_matrix);
     
             this.child1_.sprite_.rotation = this.rotation_angle_;
-            let pos = vec3.fromValues(200, 200, 1);
+            let pos = vec3.fromValues(150, 150, 1);
             vec3.transformMat3(pos, pos, parent_transformation);
     
             this.child1_.sprite_.x = pos[0];
             this.child1_.sprite_.y = pos[1];
 
-
-            
 
             let parent_transformation2 = mat3.fromValues(1, 0, 0, 0, 1, 0, this.child1_.sprite_.x, this.child1_.sprite_.y, 1);
 
@@ -102,12 +101,6 @@ export class SceneHierarchy extends Container {
     
             this.child2_.sprite_.x = pos2[0];
             this.child2_.sprite_.y = pos2[1];
-
-
-
-
-
-
 
 
             let parent_transformation4 = mat3.fromValues(1, 0, 0, 0, 1, 0, this.child2_.sprite_.x, this.child2_.sprite_.y, 1);
