89f66c88eff8d00e24e2eaab3007bdf295ad7705
miwe95 <miweiss95@gmail.com>
2022-06-08 15:52:46 +0200

    aDDED COMMENTAR

--- src/Voronoi.ts
+++ src/Voronoi.ts
@@ -2,8 +2,6 @@ import { vec2 } from "gl-matrix";
 import { Container, Graphics, InteractionEvent } from "pixi.js";
 import { EnemyHandler } from "./EnemyHandler";
 
-
-//@ts-ignore
 var perlin = require("./perlin.js");
 
 export class Voronoi extends Container {
@@ -41,105 +39,7 @@ export class Voronoi extends Container {
         EnemyHandler.getInstance().voronoi = this;
     }
 
-    breakCircleByPlayer(impact_point: vec2) {
-        this.impact_point = vec2.fromValues(impact_point[0], impact_point[1]);
-        this.calculateSeedPoints();
-        this.calculateCells();
-        this.hit = true;
-    }
-
-    breakCirclebyClick = (_e: InteractionEvent) => {
-
-        //console.log(this.impact_point);
-        this.impact_point = vec2.fromValues(_e.data.global.x, _e.data.global.y);
-        this.calculateSeedPoints();
-        this.calculateCells();
-    }
-
-    update(_dt: number) {
-        if(this.hit)
-            this.circle.x += 100 * _dt / 1000;
-    }
-
-    //@ts-ignore
-    private generateRandomColor() {
-        //@ts-ignore
-        return '0x' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0') as number;
-    }
-
-    private drawPoint(x: number, y: number, color: number, alpha: number = 1) {
-        this.circle.beginFill(color, alpha);
-        this.circle.drawCircle(x, y, 1);
-        this.circle.endFill();
-    }
-
-    private findClosestPoints(current_position: vec2): Array<vec2> {
-        let closest_cell_center = vec2.fromValues(this.impact_point[0], this.impact_point[1]);
-        let second_closest_cell_center = vec2.fromValues(this.impact_point[0], this.impact_point[1]);
-        for (let cell_center of this.cells.keys()) {
-            if (vec2.distance(cell_center, current_position) < vec2.distance(current_position, closest_cell_center)) {
-                second_closest_cell_center = closest_cell_center;
-                closest_cell_center = cell_center;
-
-            }
-        }
-        return [closest_cell_center, second_closest_cell_center];
-    }
-
-    private calculateCells() {
-        let point_center = vec2.fromValues(Math.round(this.screen_width * 0.5), Math.round(this.screen_height * 0.1));
-        let current_position: vec2 = vec2.fromValues(0, 0);
-        perlin.noise.seed(Math.random());
-
-        for (let y = point_center[1] - this.radius; y < point_center[1] + this.radius; y++)
-            for (let x = point_center[0] - this.radius; x < point_center[0] + this.radius; x++) {
-
-                current_position = vec2.fromValues(x, y);
-
-                if (vec2.distance(current_position, point_center) < this.radius) {
-                    let closest_points = this.findClosestPoints(current_position);
-                    let closest_cell_center = closest_points[0];
-                    let second_closest_cell_center = closest_points[1];
-
-                    let m: vec2 = vec2.fromValues(0, 0);
-                    vec2.add(m, second_closest_cell_center, closest_cell_center);
-                    m[0] = (m[0] / 2);
-                    m[1] = (m[1] / 2);
-
-                    //this.drawPoint(m[0], m[1], 0xFF99FF)
-
-                    let x_m: vec2 = vec2.fromValues(0, 0);
-                    let pb_pa: vec2 = vec2.fromValues(0, 0);
-
-                    vec2.sub(x_m, current_position, m);
-                    vec2.sub(pb_pa, second_closest_cell_center, closest_cell_center);
-                    let pb_pa_abs = vec2.distance(second_closest_cell_center, closest_cell_center);
-                    let pb_pa_div: vec2 = vec2.fromValues(0, 0);
-                    pb_pa_div[0] = (pb_pa[0] / pb_pa_abs);
-                    pb_pa_div[1] = (pb_pa[1] / pb_pa_abs);
-
-                    let d: number = vec2.dot(x_m, pb_pa_div);
-
-                    //@ts-ignore
-
-                    var nooise_value = perlin.noise.simplex2(x / 100, y / 100);
-                    this.apply_noise ? d += Math.abs(nooise_value * 2) : d;
-                    //console.log(value);
-
-
-                    if (Math.floor(Math.abs(d)) == 0) {
-                        this.drawPoint(current_position[0], current_position[1], 0x20edf7)
-                    }
-                    else {
-                        if (this.show_distance_field) {
-                            this.colorDistanceField(Math.abs(d), current_position);
-                        }
-                    }
-                    //console.log(d);
-                }
-            }
-    }
-
+    //calculates 20 seedpoints around the impact point, if points are outside the circle, they are deleted
     private calculateSeedPoints() {
 
         let point_center = vec2.fromValues(this.screen_width * 0.5, this.screen_height * 0.1);
@@ -159,7 +59,69 @@ export class Voronoi extends Container {
             }
         }
     }
-    //@ts-ignore
+
+     //calculates the voronoi cells
+     private calculateCells() {
+        let point_center = vec2.fromValues(Math.round(this.screen_width * 0.5), Math.round(this.screen_height * 0.1));
+        let current_position: vec2 = vec2.fromValues(0, 0);
+        perlin.noise.seed(Math.random());
+
+        for (let y = point_center[1] - this.radius; y < point_center[1] + this.radius; y++)
+            for (let x = point_center[0] - this.radius; x < point_center[0] + this.radius; x++) {
+
+                current_position = vec2.fromValues(x, y);
+
+                if (vec2.distance(current_position, point_center) < this.radius) {
+                    let closest_points = this.findClosestPoints(current_position);
+                    let closest_cell_center = closest_points[0];
+                    let second_closest_cell_center = closest_points[1];
+
+                    let m: vec2 = vec2.fromValues(0, 0);
+                    vec2.add(m, second_closest_cell_center, closest_cell_center);
+                    m[0] = (m[0] / 2);
+                    m[1] = (m[1] / 2);
+
+                    let x_m: vec2 = vec2.fromValues(0, 0);
+                    let pb_pa: vec2 = vec2.fromValues(0, 0);
+
+                    vec2.sub(x_m, current_position, m);
+                    vec2.sub(pb_pa, second_closest_cell_center, closest_cell_center);
+                    let pb_pa_abs = vec2.distance(second_closest_cell_center, closest_cell_center);
+                    let pb_pa_div: vec2 = vec2.fromValues(0, 0);
+                    pb_pa_div[0] = (pb_pa[0] / pb_pa_abs);
+                    pb_pa_div[1] = (pb_pa[1] / pb_pa_abs);
+
+                    let d: number = vec2.dot(x_m, pb_pa_div);
+
+                    var nooise_value = perlin.noise.simplex2(x / 100, y / 100);
+                    this.apply_noise ? d += Math.abs(nooise_value * 2) : d;
+
+                    if (Math.floor(Math.abs(d)) == 0) {
+                        this.drawPoint(current_position[0], current_position[1], 0x20edf7)
+                    }
+                    else {
+                        if (this.show_distance_field) {
+                            this.colorDistanceField(Math.abs(d), current_position);
+                        }
+                    }
+                }
+            }
+    }
+
+    //find closest seed points to a give position
+    private findClosestPoints(current_position: vec2): Array<vec2> {
+        let closest_cell_center = vec2.fromValues(this.impact_point[0], this.impact_point[1]);
+        let second_closest_cell_center = vec2.fromValues(this.impact_point[0], this.impact_point[1]);
+        for (let cell_center of this.cells.keys()) {
+            if (vec2.distance(cell_center, current_position) < vec2.distance(current_position, closest_cell_center)) {
+                second_closest_cell_center = closest_cell_center;
+                closest_cell_center = cell_center;
+            }
+        }
+        return [closest_cell_center, second_closest_cell_center];
+    }
+
+    //Paints the distance field between 1 and 200 in different colors
     private colorDistanceField(d: number, current_position: vec2) {
         //this.drawPoint(current_position[0], current_position[1], 0xf70505 + Math.abs(d) * 2000);
 
@@ -239,6 +201,35 @@ export class Voronoi extends Container {
 
     }
 
+    //event handler when hitting the circle with the player
+    breakCircleByPlayer(impact_point: vec2) {
+        this.impact_point = vec2.fromValues(impact_point[0], impact_point[1]);
+        this.calculateSeedPoints();
+        this.calculateCells();
+        this.hit = true;
+    }
+
+    //event handler when clicking the circle
+    breakCirclebyClick = (_e: InteractionEvent) => {
+        this.impact_point = vec2.fromValues(_e.data.global.x, _e.data.global.y);
+        this.calculateSeedPoints();
+        this.calculateCells();
+    }
+
+    //moves the circle when hit by the player
+    update(_dt: number) {
+        if (this.hit)
+            this.circle.x += 100 * _dt / 1000;
+    }
+
+    //draws a point on a given position
+    private drawPoint(x: number, y: number, color: number, alpha: number = 1) {
+        this.circle.beginFill(color, alpha);
+        this.circle.drawCircle(x, y, 1);
+        this.circle.endFill();
+    }
+
+    //Interpolates between two color strings and return hex color
     private lerpColor(a: string, b: string, amount: number) {
 
         var ah = +a.replace('#', '0x'),
